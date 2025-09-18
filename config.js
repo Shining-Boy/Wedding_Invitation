4c2d78b394dc2871a11d74227ca59c38e4e035e6
hsbae <hellospring89@gmail.com>
2025-09-18 21:13:47 +0900

    카카오톡 공유 이미지 수정3

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 export const WEDDING_INVITATION_URL = "https://symphonious-moxie-1ae0a3.netlify.app/"; //"http://localhost:8000/";
 export const KAKAOTALK_API_TOKEN = "9c0e621465bf328dcc03f237deabba5d";
-export const KAKAOTALK_SHARE_IMAGE = "./src/assets/wedding_photo01.jpg";
+export const KAKAOTALK_SHARE_IMAGE = 
   // "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+  "https://drive.google.com/uc?export=view&id=1bkMdiRicUvFn-Fx0HpFTc8Qy0kxBEo09";
 
 export const WEDDING_DATE = "2025년 10월 18일, 토요일 낮 12시";
 export const WEDDING_LOCATION = "청주 갤러리카페111";
